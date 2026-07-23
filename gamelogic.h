191b9110f8da34c60f2cb696a7c1f0b030b7afc8
main()function <madhavkohli37@gmail.com>
2026-07-23 12:52:58 +0530

    Update gamelogic.h

--- gamelogic.h
+++ gamelogic.h
@@ -2,31 +2,6 @@
 class GameLogic {
 public:
     int safeSpots[8] = {0, 8, 13, 21, 26, 34, 39, 47};
-
-    int trackRow[52] = {
-        0, 0, 0, 1, 2, 2,
-        3, 4, 5, 5, 5, 5,
-        5, 5, 6, 6, 6, 6,
-        7, 7, 7, 7, 8, 9,
-        10, 10, 11, 12, 12, 12,
-        11, 10, 9, 8, 7, 7,
-        7, 7, 6, 6, 6, 6,
-        5, 5, 5, 5, 4, 3,
-        2, 2, 1, 0
-    };
-
-    int trackCol[52] = {
-        13, 15, 17, 17, 15, 13,
-        13, 13, 13, 15, 17, 19,
-        21, 23, 23, 21, 19, 17,
-        17, 15, 13, 11, 11, 11,
-        11, 13, 13, 13, 15, 17,
-        17, 17, 17, 17, 17, 15,
-        13, 11, 11, 13, 15, 17,
-        17, 15, 13, 11, 11, 11,
-        13, 15, 15, 13
-    };
-
     bool isSafe(int absPos) {
         for (int i = 0; i < 8; i++) {
             if (safeSpots[i] == absPos) return true;
@@ -98,91 +73,18 @@ public:
 
         return false;
     }
-
-    // ============ BOARD DISPLAY WITH TOGGLE ============
-
-    void showBoard(Player players[], int numPlayers, vector<string> &baseBoard) {
-        vector<string> board = baseBoard;
-
-        for (int p = 0; p < numPlayers; p++) {
-            Player &player = players[p];
-            char symbol = player.house;
-
-            for (int i = 0; i < 4; i++) {
-                Pawn &pawn = player.pawns[i];
-                int row = -1, col = -1;
-
-                if (pawn.position == -1) {
-                    if (player.house == 'A') {
-                        int basePos[4][2] = {{0,2}, {0,6}, {2,2}, {2,6}};
-                        row = basePos[i][0]; col = basePos[i][1];
-                    }
-                    else if (player.house == 'B') {
-                        int basePos[4][2] = {{0,22}, {0,26}, {10,22}, {10,26}};
-                        row = basePos[i][0]; col = basePos[i][1];
-                    }
-                    else if (player.house == 'C') {
-                        int basePos[4][2] = {{10,22}, {10,26}, {12,22}, {12,26}};
-                        row = basePos[i][0]; col = basePos[i][1];
-                    }
-                    else if (player.house == 'D') {
-                        int basePos[4][2] = {{10,2}, {10,6}, {12,2}, {12,6}};
-                        row = basePos[i][0]; col = basePos[i][1];
-                    }
-                }
-                else if (pawn.position >= 0 && pawn.position <= 50) {
-                    int absPos = getAbsPos(player, pawn.position);
-                    row = trackRow[absPos];
-                    col = trackCol[absPos];
-                }
-                else if (pawn.position >= 51 && pawn.position < 56) {
-                    int homeRow[4][5] = {
-                        {6, 6, 6, 6, 6},
-                        {6, 6, 6, 6, 6},
-                        {6, 6, 6, 6, 6},
-                        {6, 6, 6, 6, 6}
-                    };
-                    int homeCol[4][5] = {
-                        {1, 2, 3, 4, 5},
-                        {19, 20, 21, 22, 23},
-                        {19, 20, 21, 22, 23},
-                        {1, 2, 3, 4, 5}
-                    };
-                    int playerIdx = player.house - 'A';
-                    int stretchIdx = pawn.position - 51;
-                    row = homeRow[playerIdx][stretchIdx];
-                    col = homeCol[playerIdx][stretchIdx];
-                }
-                else if (pawn.isHome) {
-                    int centerPos[4][2] = {{5,13}, {5,15}, {6,13}, {6,15}};
-                    int playerIdx = player.house - 'A';
-                    row = centerPos[playerIdx][0];
-                    col = centerPos[playerIdx][1];
-                }
-
-                if (row >= 0 && row < board.size() && col >= 0 && col < board[row].length()) {
-                    board[row][col] = symbol;
-                }
-            }
-        }
-
-        cout << "\n========== CURRENT BOARD ==========\n\n";
-        for (string &line : board) {
-            cout << line << "\n";
-        }
-        cout << "\n===================================\n";
-    }
-
     // NEW: Ask if player wants to see board, then show it
-    void askAndShowBoard(Player players[], int numPlayers, vector<string> &baseBoard) {
-        int choice;
+    void askAndShowBoard( vector<string> &v) {
+        bool choice;
         cout << "\nShow board? (1 = Yes, 0 = No): ";
         cin >> choice;
         cin.ignore();
 
-        if (choice == 1) {
-            showBoard(players, numPlayers, baseBoard);
-        }
+        if (choice == true) {
+           for(string s : v) {
+            cout << s<< endl;
+           }
+         }
     }
     // ============ CAPTURE (TRAP) LOGIC ============
 
