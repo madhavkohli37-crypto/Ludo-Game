daef7e612e07ca246c362b206b8d3f0d8292adee
main()function <madhavkohli37@gmail.com>
2026-07-30 00:04:51 +0530

    Update turn.h

--- turn.h
+++ turn.h
@@ -4,6 +4,9 @@
 #include<cstring>
 #include<climits>
 using namespace std; 
+void delay(int ms) {
+    for (volatile long long i = 0; i < ms * 800000LL; i++);
+}
 vector<int>F(int currentplayer,int t6,vector<int>&numbers) {
     cout << "Press Enter to roll the dice --> ";
     cin.get();
@@ -34,10 +37,10 @@ vector<int> turnAI(int currentplayer) {
         int diceRoll = (rand() % 6) + 1;
         numbers.push_back(diceRoll);
         cout << "\nAI Player " << currentplayer << " rolled a " << diceRoll << "!\n";
-        
         if (diceRoll == 6 && t6 < 2) {
             t6++;
             cout << "AI got a 6! Rolling again...\n";
+            delay(1000);
         }
         else if (diceRoll == 6) {
             cout << "AI got 3 sixes in a row! Turn terminated!\n";
