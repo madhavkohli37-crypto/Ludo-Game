2f0cc0542a75130f8727c45a7d9495d94cf18521
main()function <madhavkohli37@gmail.com>
2026-07-31 20:52:53 +0530

    Update pawn.h

--- pawn.h
+++ pawn.h
@@ -1,8 +1,8 @@
 class Pawn {
 public:
-    int position;      
-    bool isHome;       
-    bool isActive;     
+    int position;      // -1 = in base, 0-56 = track/home stretch
+    bool isHome;       // true if reached center H (position 56)
+    bool isActive;     // true if on track
 
     Pawn() {
         position = -1;
