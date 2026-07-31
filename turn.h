f6049e3657cf9320758a9c6e9eae164b377a1667
main()function <madhavkohli37@gmail.com>
2026-07-31 20:53:24 +0530

    Update turn.h

--- turn.h
+++ turn.h
@@ -1,34 +1,42 @@
 #include <iostream>
-#include<vector>
-#include<ctime>
-#include<cstring>
-#include<climits>
+#include <vector>
+#include <ctime>
+#include <cstring>
+#include <climits>
+#include <thread>
+#include <chrono>
+
 using namespace std; 
 void delay(int ms) {
-    for (volatile long long i = 0; i < ms * 800000LL; i++);
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
-vector<int>F(int currentplayer,int t6,vector<int>&numbers) {
+
+vector<int> F(int currentplayer, int t6, vector<int>& numbers) {
     cout << "Press Enter to roll the dice --> ";
     cin.get();
-    int diceRoll=(rand()%6)+1;
+    
+    int diceRoll = (rand() % 6) + 1;
     numbers.push_back(diceRoll);
     cout << "\nPlayer " << currentplayer << " rolled a " << diceRoll << "!\n";
-    if(diceRoll==6 and t6<2) {
+    
+    if (diceRoll == 6 && t6 < 2) {
         t6++;
-        cout<<"You got a 6 so you can roll again\n";
-        F(currentplayer,t6,numbers);
+        cout << "You got a 6 so you can roll again\n";
+        F(currentplayer, t6, numbers);
     }
-    else if(diceRoll==6) {
-        cout<<"you got 3 6's in a row hence your turn is terminated!\n";
+    else if (diceRoll == 6) {
+        cout << "You got 3 sixes in a row hence your turn is terminated!\n";
         numbers.clear();
     }
-        return numbers;
+    return numbers;
 }
+
 vector<int> turn(int currentplayer) {
-    int t6=0;
-vector<int>numbers;
-    return F(currentplayer,t6,numbers);
+    int t6 = 0;
+    vector<int> numbers;
+    return F(currentplayer, t6, numbers);
 }
+
 vector<int> turnAI(int currentplayer) {
     int t6 = 0;
     vector<int> numbers;
@@ -37,6 +45,7 @@ vector<int> turnAI(int currentplayer) {
         int diceRoll = (rand() % 6) + 1;
         numbers.push_back(diceRoll);
         cout << "\nAI Player " << currentplayer << " rolled a " << diceRoll << "!\n";
+        
         if (diceRoll == 6 && t6 < 2) {
             t6++;
             cout << "AI got a 6! Rolling again...\n";
